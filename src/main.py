182e2a04c49bf39d1f4cb1ce9046f7dd5b8accac
fieryhenry <henry+dev@battlecatsmodding.org>
2025-11-08 16:15:01 +0000

    fronten

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    filename = request.form.get("filename")
+    print(filename)
+    return {"genre": "foo", "tempo": 120}
 
 
 if __name__ == "__main__":
